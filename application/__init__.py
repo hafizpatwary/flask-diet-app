37c4ec26138c38caf7b24a8e7f4fae30ed19ba4d
hafizpatwary <hafiz9711@gmail.com>
2019-11-28 22:39:20 +0000

    connected to database instance, and set environment variable (mac)

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://'+os.getenv('MYSQL_USERNAME')+':'+os.getenv('MYSQL_PASSWORD')+'@34.89.111.107/diet'
+db = SQLAlchemy(app)
 
 from application import routes
