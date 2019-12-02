cbc1b8139ed44d442ce3187f33fe1091d90d0515
hafizpatwary <hafiz9711@gmail.com>
2019-12-02 14:03:11 +0000

    Added from for user registration

--- application/forms.py
+++ application/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
-from wtforms.validators import DataRequired, Length, NumberRange
-
+from wtforms import StringField, IntegerField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Length, NumberRange, Email, EqualTo, ValidationError
+from application.models import User 
 
 class DietForm(FlaskForm):
     diet_name = StringField('Diet name', validators=[DataRequired(),Length(min=1, max=128)])
@@ -14,4 +14,20 @@ class FoodForm(FlaskForm):
     food_name = StringField('Food', validators=[DataRequired(), Length(min=1, max=128)])
     calories = IntegerField('Calories kcal', validators=[NumberRange(min=0, max=100000)])
     submit = SubmitField('Create Food')
+
+
+
+class RegistrationForm(FlaskForm):
+	name = StringField('Name', validators=[DataRequired(),Length(min=1, max=64)])
+	surname = StringField('Surname', validators=[DataRequired(),Length(min=1, max=64)])
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	confirm_password = PasswordField(' Confrim Password', validators=[DataRequired(), EqualTo('password')])
+	submit = SubmitField('Sign Up')
+
+	def validate_email(self, email):
+		user = User.query.filter_by(email=email.data).first()
+
+		if user:
+			raise ValidationError('Email is already in use!')
 
