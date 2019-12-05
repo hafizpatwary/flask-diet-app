cf8a6c181c78a3c9b5b737d6dbd9bd296c6802d2
= <hafiz9711@python-project.europe-west2-c.c.avian-scanner-258809.internal>
2019-12-05 10:29:55 +0000

    Three test in working conditions

--- tests/test_backend_test.py
+++ tests/test_backend_test.py
@@ -4,7 +4,7 @@ from flask import abort, url_for
 from flask_testing import TestCase
 import os
 from application import app, db
-from application.models import User, Diet
+from application.models import User, Diet, Food
 
 
 class TestBase(TestCase):
@@ -25,22 +25,14 @@ class TestBase(TestCase):
         db.create_all()
 
         # create test admin user
-
         admin = User(name="admin", surname="admin", email="admin@admin.com", password="admin2019")
 
         # create test non-admin user
         trainer = User(name="test", surname="user", email="test@user.com", password="test2019")
 
-        admin = User(name="admin", surname="admin", email="admin@admin.com", password="admin2016")
-
-        # create test non-admin user
-        employee = User(name="test", surname="user", email="test@user.com", password="test2016")
-
-
         # save users to database
         db.session.add(admin)
-        db.session.add(employee)
-
+        db.session.add(trainer)
 
         # create test food apple
         apple = Food(food_name='Apple', calories=75)
@@ -53,18 +45,16 @@ class TestBase(TestCase):
         db.session.add(pasta)
 
         # create a diet for admin
-        vegan_diet = Diet(diet_name='Vegan', description='No meat, eggs or milk', userID=1)
+        vegan_diet = Diet(diet_name='Vegan', description='No meat, eggs or milk', user=admin)
 
         # create a 2nd diet for admin
-        mass_diet = Diet(diet_name='Mass gainer', description='Protein mainly', userID=1)
+        mass_diet = Diet(diet_name='Mass gainer', description='Protein mainly', user=admin)
         
         # save diets to database
         db.session.add(vegan_diet)
         db.session.add(mass_diet)
 
         # commit all changes to database
-
-            
         db.session.commit()
 
     def tearDown(self):
@@ -75,7 +65,6 @@ class TestBase(TestCase):
         db.drop_all()
 
 
-
 class test_models(TestBase):
     def test_diet_model(self):
         """ Create a diet """
@@ -84,23 +73,22 @@ class test_models(TestBase):
         self.assertEqual(Diet.query.count(), 2)
 
         # Test if the first diet is called Vegan
-        veganDiet = Diet.query.filter_by(dietID=1).first()
-        self.assertEqual(veganDiet.diet_name, 'Vegan')
+        vegan_diet = Diet.query.filter_by(dietID=1).first()
+        self.assertEqual(vegan_diet.diet_name, 'Vegan')
 
-        self.assertEqual(veganDiet.user, admin)
+        self.assertEqual(vegan_diet.user.name, 'admin')
 
 
 
     def test_food_model(self):
-        """ Create a diet """
-        food = Food(food_name='Apple', calories=75)
+        """ Test food query works """
 
-        db.session.add(food)
-        db.session.commit()
+        # Test there is only two food in food table
+        self.assertEqual(Food.query.count(), 2)
 
-        self.assertEqual(Food.query.count(), 1)
+        # Test food table is read correctly
         apple = Food.query.filter_by(foodID=1).first()
-        self.assertEqual(veganDiet.diet_name, 'Apple')
+        self.assertEqual(apple.food_name, 'Apple')
 
 
 
@@ -110,17 +98,128 @@ class test_models(TestBase):
 
 
 
-class test_diet(TestBase):
-    def test_diet_model(self):
-        diet = Diet(diet_name='Vegan', description='No meat, eggs or milk', userID=1)
 
-        db.session.add(diet)
-        db.session.commit()
 
-        self.assertEqual(Diet.query.count(), 1)
-        veganDiet = Diet.query.filter_by(dietID=1).first()
-        self.assertEqual(veganDiet.diet_name, 'Vegan')
 
-    def test_food_model(self):
-        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
